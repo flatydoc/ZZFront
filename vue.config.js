d7350269283e42b04eba0625fa946c880c0f1498
Macbook <flatydoc@gmail.com>
2022-12-13 02:16:50 +0200

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "/ZZFront/",
+  publicPath: process.env.NODE_ENV === "production" ? "/ZZFront/" : "/",
 });
